8555ebb790193c44d58698055eb9aec3645412c2
simon-p-r <simon.ricaldone@circabs.com>
2016-03-04 23:15:16 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# yajwt [![Build Status](https://secure.travis-ci.org/auth0/node-jsonwebtoken.png)](http://travis-ci.org/auth0/node-jsonwebtoken)
+
+
+An implementation of [JSON Web Tokens](https://tools.ietf.org/html/rfc7519).
+
+This was developed against `draft-ietf-oauth-json-web-token-08`. It makes use of [node-jws](https://github.com/brianloveswords/node-jws) and has heavily used [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken) module as inspiration
+
+# Install
+
+```bash
+$ npm install yajwt
+```
+
+# Usage
+
+### jwt.sign(options, [callback])
+
+(Asynchronous) If a callback is supplied, function will return asynchronously
+
+(Synchronous) Returns an object with an error(on failure) and token property (on success)
+
+
+`options`:
+
+* `header` object with following properties
+  * `alg` default: `RS256`
+  * `typ` default 'JWT', this is only accepted value for this property
+* `payload`: object with the following properties
+  * `aud`: string - audience of token
+  * `exp`: expressed in seconds or a string describing a time span [rauchg/ms](https://github.com/rauchg/ms.js). Eg: `60`, `"2 days"`, `"10h"`, `"7d"`
+  * `iat`: same as above, defaults to the moment payload is signed
+  * `iss`: string -  issuer of token
+  * `jti`: string - unique identity of token
+  * `nbf`: expressed in seconds or a string describing a time span [rauchg/ms](https://github.com/rauchg/ms.js). Eg: `60`, `"2 days"`, `"10h"`, `"7d"`
+  * `sub`: string - describing subject of token
+  * `privateKey`: string or buffer of private key to sign token
+
+
+
+
+Additional custom header properties can be provided via the `header` object.
+
+
+Example
+
+```js
+
+const jwt = require('yajwt');
+
+// read key for signing
+const key = fs.readFileSync('private.pem');  
+const token = jwt.sign({ header: { alg: 'HS256' }, payload: {aud: 'private'}, privateKey: key });
+
+
+// sign asynchronously
+jwt.sign({ header: { alg: 'HS256' }, payload: {aud: 'private'}, privateKey: key }, (err, token) => {
+  console.log(err, token);
+});
+```
+
+### jwt.verify(options, callback)
+
+(Asynchronous) If a callback is supplied, function acts asynchronously. Callback has err, valid signature
+(Synchronous) If a callback is not supplied, function acts synchronously. Returns true or false if valid or not
+
+
+
+`options`
+
+* `algorithms` default: RS256.
+* `signature` json string to verify
+* `publicKey`: is a string or buffer containing either the secret for HMAC algorithms, or the PEM
+encoded public key for RSA and ECDSA.
+
+
+```js
+// verify a token asymmetric
+const publicKey = fs.readFileSync('public.pem');  // get public key
+
+const valid = jwt.verify(token, publicKey);
+console.log(valid) // true
+
+// verify a token symmetric
+jwt.verify({signature: jsonString, algorithm: 'HS256',  publicKey: publicKey}, function(err, valid) {
+  console.log(err) // null
+  console.log(valid) //true
+});
+```
+
+Todo
+
+* improve timestamps
+* improve api
+* improve error handling
+* improve docs
---
+++ test/index.js
@@ -0,0 +1,243 @@
+'use strict';
+
+const Code = require('code');
+const Fs = require('fs');
+const Jwt = require('../lib/index.js');
+const Lab = require('lab');
+const Os = require('os');
+const Path = require('path');
+
+// fixtures
+const PrivateKey = Fs.readFileSync(Path.resolve(__dirname, './fixtures/private.pem'));
+const PublicKey = Fs.readFileSync(Path.resolve(__dirname, './fixtures/public.pem'));
+
+
+// Set-up lab
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+
+const verifyOptions = (token) => {
+
+    return {
+        signature: token,
+        publicKey: PublicKey,
+        algorithm: 'RS256'
+    };
+
+};
+
+const signingOptions = () => {
+
+    return {
+        header: {
+            alg: 'RS256',
+            // token type
+            typ: 'JWT'
+        },
+        // payload can be object, buffer or string
+        payload: {
+            exp: '365d',
+            nbf: '1d',
+            host: Os.hostname(),
+            port: 3000
+        },
+        privateKey: PrivateKey
+    };
+};
+
+
+
+describe('Jwt', () => {
+
+
+    it('should sign a payload sync', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        expect(result.token).to.be.a.string();
+        expect(result.error).to.not.exist();
+        done();
+
+    });
+
+    it('should return an error when signing a payload sync due to missing algorithm', (done) => {
+
+        const ops = signingOptions();
+        ops.header.alg = null;
+        const result = Jwt.sign(ops);
+        expect(result.token).to.be.undefined();
+        expect(result.error).to.exist();
+        done();
+
+    });
+
+    it('should sign a payload async', (done) => {
+
+        Jwt.sign(signingOptions(), (err, token) => {
+
+            expect(err).to.not.exist();
+            expect(token).to.be.a.string();
+            done();
+
+        });
+
+    });
+
+    it('should return an error when signing a payload async due to missing algorithm', (done) => {
+
+        const ops = signingOptions();
+        ops.header.alg = null;
+        Jwt.sign(ops, (err, token) => {
+
+            expect(err).to.exist();
+            expect(token).to.not.exist();
+            done();
+
+        });
+
+    });
+
+    it('should verify a payload sync', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        const ops = verifyOptions(result.token);
+        const valid = Jwt.verify(ops);
+        expect(valid).to.be.true();
+        done();
+
+    });
+
+    it('should return false when verify a payload sync due to missing signature', (done) => {
+
+        const ops = verifyOptions();
+        const valid = Jwt.verify(ops);
+        expect(valid).to.be.false();
+        done();
+
+    });
+
+    it('should return false when verify a payload sync due to an invalid signature', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        const ops = verifyOptions(result.token);
+        ops.signature = 'This is invalid';
+        const valid = Jwt.verify(ops);
+        expect(valid).to.be.false();
+        done();
+
+    });
+
+
+    it('should verify a payload async', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        const ops = verifyOptions(result.token);
+        Jwt.verify(ops, (err, token) => {
+
+            expect(err).to.not.exist();
+            expect(token).to.be.true();
+            done();
+
+        });
+
+    });
+
+    it('should return an error when verifying a payload async due to missing signature', (done) => {
+
+        const ops = verifyOptions();
+        Jwt.verify(ops, (err, token) => {
+
+            expect(err).to.exist();
+            expect(token).to.be.null();
+            done();
+
+        });
+
+    });
+
+    it('should return an error when verifying a payload async due to invalid signature', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        const ops = verifyOptions(result.token);
+        ops.signature = 'This is an invalid token';
+        Jwt.verify(ops, (err, token) => {
+
+            expect(err).to.exist();
+            expect(token).to.be.null();
+            done();
+
+        });
+
+    });
+
+    it('should return an error when verifying a payload async due to invalid timestamps', (done) => {
+
+        const signOps = signingOptions();
+        signOps.payload.exp = (Math.floor(Date.now() / 1000)) - 1;
+        const result = Jwt.sign(signOps);
+        const ops = verifyOptions(result.token);
+        Jwt.verify(ops, (err, token) => {
+
+            expect(err).to.exist();
+            expect(token).to.be.null();
+            done();
+
+        });
+
+    });
+
+    it('should fail verfiy on timestamp checking', (done) => {
+
+        const signOps = signingOptions();
+        signOps.payload.exp = 1000;
+        signOps.payload.iat = 2000000000;
+        delete signOps.payload.nbf;
+        const result = Jwt.sign(signOps);
+        const ops = verifyOptions(result.token);
+        const verify = Jwt.verify(ops);
+        expect(verify).to.be.false();
+        done();
+
+    });
+
+    it('should fail verfiy on timestamp checking due to invalid nbf', (done) => {
+
+        const signOps = signingOptions();
+        signOps.payload.nbf = 2000000000;
+        const result = Jwt.sign(signOps);
+        const ops = verifyOptions(result.token);
+        const verify = Jwt.verify(ops);
+        expect(verify).to.be.false();
+        done();
+
+    });
+
+    it('should pass verfiy on timestamp checking', (done) => {
+
+        const signOps = signingOptions();
+        signOps.payload.iat = '1d';
+        delete signOps.payload.exp;
+        const result = Jwt.sign(signOps);
+        const ops = verifyOptions(result.token);
+        const verify = Jwt.verify(ops);
+        expect(verify).to.be.true();
+        done();
+
+    });
+
+    it('should return a decoded json web token', (done) => {
+
+        const result = Jwt.sign(signingOptions());
+        const decoded = Jwt.decode(result.token);
+        expect(decoded).to.be.an.object();
+        expect(decoded.header.alg).to.equal('RS256');
+        expect(decoded.payload.host).to.equal(Os.hostname());
+        done();
+
+    });
+
+
+
+});
